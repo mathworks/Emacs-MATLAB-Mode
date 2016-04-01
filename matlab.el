a5130509944b3a1e43149e00e31d364791326d82
Uwe Brauer <oub@mat.ucm.es>
2016-04-01 08:08:07 +0000

    * matlab.el (matlab-enable-block-highlighting): Applied patch provided by kaushal_modi@users.sf.net
    * matlab.el (matlab-mode-version): change version to 3.3.5

--- matlab.el
+++ matlab.el
@@ -7,7 +7,7 @@
 ;; Keywords: MATLAB(R)
 ;; Version:
 
-(defconst matlab-mode-version "3.3.4"
+(defconst matlab-mode-version "3.3.5"
   "Current version of MATLAB(R) mode.")
 
 ;;
@@ -3739,8 +3739,8 @@ If ARG is nil, then highlighting is toggled."
 			  post-command-hook)
 		  -1 1)))
     (if (> arg 0)
-	(add-hook 'post-command-hook 'matlab-start-block-highlight-timer)
-      (remove-hook 'post-command-hook 'matlab-start-block-highlight-timer))))
+	(add-hook 'post-command-hook 'matlab-start-block-highlight-timer nil :local)
+      (remove-hook 'post-command-hook 'matlab-start-block-highlight-timer :local))))
 
 (defvar matlab-block-highlight-overlay nil
   "The last highlighted overlay.")
