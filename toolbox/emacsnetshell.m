a99727d462978354e94d24d838a3cf822fd012af
Eric Ludlam <eric@siege-engine.com>
2019-11-24 20:29:49 -0500

    toolbox/emacsnetshell.m:
    Don't send 'init' twice if user calls this the first time with 'init'
    as an input.

--- toolbox/emacsnetshell.m
+++ toolbox/emacsnetshell.m
@@ -1,13 +1,13 @@
 function emacsnetshell(cmd, data)
 % Create a connection to an EMACS editor server.
 % If we succeed then setup a timer to watch what comes in from the connection.
-    
+
     EMACSSERVER = getappdata(groot, 'EmacsNetShell');
-    
+
     if nargin == 0
         cmd = 'init';
     end
-   
+
     if strcmp(cmd, 'shutdown')
         % Shutdown our connection to emacs.
         setappdata(groot, 'EmacsNetShell', []);
@@ -20,22 +20,26 @@ function emacsnetshell(cmd, data)
             EMACSSERVER = [];
             setappdata(groot, 'EmacsNetShell', EMACSSERVER);
         end
-        
+
         if isempty(EMACSSERVER)
             EMACSSERVER = emacs.EmacsServer();
             setappdata(groot, 'EmacsNetShell', EMACSSERVER);
+            sendinit = true;
+        else
+            sendinit = false;
         end
 
         if ~ischar(cmd)
             error('Command must be a char vector.');
         end
-        
-        if nargin == 2
-            EMACSSERVER.SendCommand(cmd, data);
-        else
-            EMACSSERVER.SendCommand(cmd);
-        end            
-    end
 
+        if ~strcmp(cmd,'init') && sendinit
+            if nargin == 2
+                EMACSSERVER.SendCommand(cmd, data);
+            else
+                EMACSSERVER.SendCommand(cmd);
+            end
+        end
+    end
 
 end
