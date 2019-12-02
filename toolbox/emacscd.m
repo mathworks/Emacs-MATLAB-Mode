49338f89794dde60ce0b5bbcc53ce2944f0c6723
Eric Ludlam <eric@siege-engine.com>
2019-12-01 21:15:57 -0500

    toolbox/emacscd.m
    Add example code for identifying if you're inside Emacs for this ML session
    and sending the <EMACSCAP> cookie only if we are in Emacs.

--- toolbox/emacscd.m
+++ toolbox/emacscd.m
@@ -2,9 +2,18 @@ function emacscd(dir)
 % CD to DIRECTORY in a way that Emacs Shell won't see via dirtrack.
 % Instead, show example of how to tell Emacs what the new directory is.
     
-    cd(dir);
+    if nargin == 1
+        cd(dir);
+    end
+
+    emacs = getenv('INSIDE_EMACS');
     
-    disp('<EMACSCAP>(eval)')
-    disp(['(setq default-directory "' pwd '")'])
-    disp('</EMACSCAP>')
+    if isempty(emacs)
+        disp('Not inside Emacs')
+    else
+        % disp('Inside Emacs - Sending directory cookie.')
+        disp('<EMACSCAP>(eval)')
+        disp(['(setq default-directory "' pwd '")'])
+        disp('</EMACSCAP>')
+    end
 end
