968f46fbd21990ec001b9d37d16e0ed5a9ad5c6b
Eric Ludlam <eric@siege-engine.com>
2019-12-20 22:22:20 -0500

    tests/testeeval.m:
    Add new case for 'noend' - where no </EMACSCAP> is sent.
    Lets you test C-c C-c binding, to make sure there is an escape hatch.

--- tests/testeeval.m
+++ tests/testeeval.m
@@ -6,19 +6,22 @@ function testeeval(style)
     end
     
     switch style
-        case 'eval'
-          disp('<EMACSCAP>(eval)')
-          disp('(setq mstest-EVAL-TEST "evaluate this")')
-          disp('</EMACSCAP>')
-          
+      case 'eval'
+        disp('<EMACSCAP>(eval)')
+        disp('(setq mstest-EVAL-TEST "evaluate this")')
+        disp('</EMACSCAP>')
+        
       case 'buffer'
-          disp('<EMACSCAP>(*MATLAB TEST*)')
-          pause(2)
-          disp('Random text to display in a buffer')
-          disp('Random text to display in a buffer')
-          disp('Random text to display in a buffer')
-          disp('Random text to display in a buffer')
-          disp('</EMACSCAP>')
+        disp('<EMACSCAP>(*MATLAB TEST*)')
+        pause(2)
+        disp('Random text to display in a buffer')
+        disp('Random text to display in a buffer')
+        disp('Random text to display in a buffer')
+        disp('Random text to display in a buffer')
+        disp('</EMACSCAP>')
+      case 'noend'
+        disp('<EMACSCAP>(*MATLAB TEST*)')
+        disp('Random text to display but can''t');
     end
 
 end
