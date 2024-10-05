a24febf630d2d6cf8dad7f7090df57a60abbcb55
Uwe Brauer <oub@mat.ucm.es>
2024-10-05 14:56:44 +0200

    Add copyright headers to remaining files.

--- toolbox/help.m
+++ toolbox/help.m
@@ -1,3 +1,17 @@
+% Copyright (C) 2024  Eric Ludlam (and others)
+
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
 function [out, docTopic] = help(varargin)
 % Provide help, augmented so Emacs picks it up to display in a special buffer.
 % See the help for the built-in help command by asking for "help help" in
